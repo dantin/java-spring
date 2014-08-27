18ce37c3fcebc8c33c4f615eefb5d763b9b79474
David Ding <chengjie.ding@gmail.com>
2014-08-27 21:47:30 +0800

    bean factory IoC demo

---
+++ src/test/java/com/demo2do/spring/ioc/beanfactory/BeanFactoryMainApp.java
@@ -0,0 +1,17 @@
+package com.demo2do.spring.ioc.beanfactory;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Test Case for BeanFactory
+ */
+public class BeanFactoryMainApp {
+
+    public static void main(String[] args) {
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("ioc/bean-factory.xml"));
+
+        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
