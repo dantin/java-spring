45688da05a3b62156cd250de2704b99f090649d7
David Ding <chengjie.ding@gmail.com>
2014-08-27 23:10:57 +0800

    prototype scope demo

---
+++ src/test/java/com/demo2do/spring/scope/prototype/PrototypeScopeMainApp.java
@@ -0,0 +1,22 @@
+package com.demo2do.spring.scope.prototype;
+
+import com.demo2do.spring.beans.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Prototype Scope Demo
+ */
+public class PrototypeScopeMainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("scope/prototype.xml");
+
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.setMessage("I'm object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+        objB.getMessage();
+    }
+}
