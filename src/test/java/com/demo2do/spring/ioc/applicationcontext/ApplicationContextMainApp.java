b2d3ff381bc5be3ae47c9627633281d6f28f27e3
David Ding <chengjie.ding@gmail.com>
2014-08-27 22:06:26 +0800

    application context IoC demo

---
+++ src/test/java/com/demo2do/spring/ioc/applicationcontext/ApplicationContextMainApp.java
@@ -0,0 +1,17 @@
+package com.demo2do.spring.ioc.applicationcontext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Application Context IoC Demo
+ */
+public class ApplicationContextMainApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc/application-context.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
